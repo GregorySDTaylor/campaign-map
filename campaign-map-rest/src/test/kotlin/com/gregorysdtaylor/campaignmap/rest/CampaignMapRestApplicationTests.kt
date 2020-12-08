8c3b432a79ea18f070f0a43569e6bfaf02e9d32e
Greg Taylor <GregorySDTaylor@gmail.com>
2020-12-08 18:58:23 -0500

    bootstrapped rest api with spring initializr

---
+++ campaign-map-rest/src/test/kotlin/com/gregorysdtaylor/campaignmap/rest/CampaignMapRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gregorysdtaylor.campaignmap.rest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CampaignMapRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
