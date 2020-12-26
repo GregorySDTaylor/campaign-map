011ed2e5069aef6c32ffeb77bf57948bdd88cdab
Greg Taylor <GregorySDTaylor@gmail.com>
2020-12-26 12:35:22 -0500

    first cors config but correct

--- src/main/kotlin/com/gregorysdtaylor/campaignmap/rest/CampaignMapRestApplication.kt
+++ src/main/kotlin/com/gregorysdtaylor/campaignmap/rest/CampaignMapRestApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class CampaignMapRestApplication
 
 fun main(args: Array<String>) {
-	runApplication<CampaignMapRestApplication>(*args)
+    runApplication<CampaignMapRestApplication>(*args)
 }
