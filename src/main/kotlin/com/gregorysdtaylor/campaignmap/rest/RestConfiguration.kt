abb8f746d6d5e9be20b074b7c98a14478db0a853
Greg Taylor <GregorySDTaylor@gmail.com>
2020-12-26 12:08:46 -0500

    cors configuration

---
+++ src/main/kotlin/com/gregorysdtaylor/campaignmap/rest/RestConfiguration.kt
@@ -0,0 +1,19 @@
+package com.gregorysdtaylor.campaignmap.rest
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+
+@Configuration
+class RestConfiguration: RepositoryRestConfigurer {
+
+    override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration, cors: CorsRegistry) {
+        cors.addMapping("/**")
+            .allowedOrigins("http://localhost:8080")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .exposedHeaders("*")
+    }
+}
