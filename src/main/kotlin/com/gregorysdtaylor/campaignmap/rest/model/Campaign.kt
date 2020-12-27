174f8211da4fd2f5e71175552602a82b703c933e
Greg Taylor <GregorySDTaylor@gmail.com>
2020-12-27 17:52:55 -0500

    added cascade type all

--- src/main/kotlin/com/gregorysdtaylor/campaignmap/rest/model/Campaign.kt
+++ src/main/kotlin/com/gregorysdtaylor/campaignmap/rest/model/Campaign.kt
@@ -1,17 +1,15 @@
 package com.gregorysdtaylor.campaignmap.rest.model
 
 import java.net.URL
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.OneToMany
+import javax.persistence.*
+import javax.persistence.CascadeType.ALL
 
 @Entity
 data class Campaign(
         @Id @GeneratedValue val id: Long,
         val name: String?,
         val description: String?,
-        @OneToMany(mappedBy = "campaign") val planets: MutableList<Planet> = mutableListOf(),
+        @OneToMany(cascade=[ALL], mappedBy = "campaign") val planets: MutableList<Planet> = mutableListOf(),
         val imageUrl: URL?,
         val mapUrl: URL?
 )
--- src/main/kotlin/com/gregorysdtaylor/campaignmap/rest/model/Planet.kt
+++ src/main/kotlin/com/gregorysdtaylor/campaignmap/rest/model/Planet.kt
@@ -2,6 +2,7 @@ package com.gregorysdtaylor.campaignmap.rest.model
 
 import java.net.URL
 import javax.persistence.*
+import javax.persistence.CascadeType.ALL
 
 @Entity
 data class Planet(
@@ -10,7 +11,7 @@ data class Planet(
         val name: String?,
         val description: String?,
         val diameterInKm: Double?,
-        @OneToMany(mappedBy = "planet") val locations: MutableList<Location> = mutableListOf(),
+        @OneToMany(cascade=[ALL], mappedBy = "planet") val locations: MutableList<Location> = mutableListOf(),
         val imageUrl: URL?,
         val mapUrl: URL?
 )
